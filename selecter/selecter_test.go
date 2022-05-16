cedb81960186b902edb6150d02a15bd1dd32a9dd
BrushXiaoMinGuo <1192962814@qq.com>
2022-05-16 21:07:43 +0800

    add select&syncpool

--- selecter/selecter_test.go
+++ selecter/selecter_test.go
@@ -1,1 +1,55 @@
 package selecter
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Test1(t *testing.T) {
+	var ch chan bool
+	ch = make(chan bool,1)
+
+	tt := time.NewTimer(2*time.Second)
+	fmt.Println(time.Now())
+	ch<-false
+
+	go func() {
+		time.Sleep(1*time.Second)
+		<-ch
+	}()
+
+	select {
+	case ch<-false:
+		fmt.Println("ok")
+		fmt.Println(time.Now())
+	case <-tt.C:
+		fmt.Println(time.Now())
+
+	}
+}
+
+func Test2(t *testing.T) {
+	var ch chan bool
+	ch = make(chan bool,1)
+
+	tt := time.NewTimer(2*time.Second)
+	fmt.Println(time.Now())
+	ch<-false
+
+	go func() {
+		time.Sleep(1*time.Second)
+		<-ch
+	}()
+
+	select {
+	case ch<-false:
+		fmt.Println("ok")
+		fmt.Println(time.Now())
+	case <-tt.C:
+		fmt.Println(time.Now())
+	default:
+		fmt.Println(time.Now())
+
+	}
+}
--- syncpooler/syncpooler_test.go
+++ syncpooler/syncpooler_test.go
@@ -1,1 +1,18 @@
 package syncpooler
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	var npool sync.Pool
+	fmt.Println(npool.Get())
+	npool.Put("str")
+	npool.Put("str1")
+	npool.Put("str3")
+	fmt.Println(npool.Get())
+	fmt.Println(npool.Get())
+	fmt.Println(npool.Get())
+}
